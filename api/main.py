364c07775020b1aa2d36d040989ab880eddb6847
engineershahid9028 <141816607+engineershahid9028@users.noreply.github.com>
2026-01-11 01:02:56 +0500

    Update main.py

--- api/main.py
+++ api/main.py
@@ -1,7 +1,26 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from api.queue import add_job
+import uuid
 
 app = FastAPI()
 
 @app.get("/")
 def root():
     return {"status": "ok", "message": "App is running"}
+
+@app.post("/job")
+async def create_job(req: Request):
+    data = await req.json()
+
+    job_id = str(uuid.uuid4())
+
+    job = {
+        "job_id": job_id,
+        "user_id": data["user_id"],
+        "msg_id": data["msg_id"],
+        "url": data["url"]
+    }
+
+    add_job(job)
+
+    return {"status": "queued", "job_id": job_id}
