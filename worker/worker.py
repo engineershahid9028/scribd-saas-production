1c5bf7e4dd453325975868e7d9699d4a122dfc80
engineershahid9028 <141816607+engineershahid9028@users.noreply.github.com>
2026-01-10 22:42:03 +0500

    Update worker.py

--- worker/worker.py
+++ worker/worker.py
@@ -2,7 +2,9 @@ import os, uuid
 from api.queue import get_job
 from worker.downloader import download_scribd
 from telegram import Bot
+from dotenv import load_dotenv
 
+load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 bot = Bot(BOT_TOKEN)
 
@@ -16,15 +18,10 @@ while True:
     user_id = job["user_id"]
     msg_id = job["msg_id"]
     url = job["url"]
-
     filename = f"{uuid.uuid4()}.pdf"
 
     def progress(p):
-        bot.edit_message_text(
-            chat_id=user_id,
-            message_id=msg_id,
-            text=f"⏳ Downloading... {p}%"
-        )
+        bot.edit_message_text(chat_id=user_id, message_id=msg_id, text=f"⏳ Downloading... {p}%")
 
     try:
         download_scribd(url, filename, progress)
@@ -33,6 +30,5 @@ while True:
     except Exception as e:
         bot.send_message(user_id, f"❌ Error: {e}")
 
-    finally:
-        if os.path.exists(filename):
-            os.remove(filename)
+    if os.path.exists(filename):
+        os.remove(filename)
