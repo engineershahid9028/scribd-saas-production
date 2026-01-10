e01c27770405733f4f727326002aae1a2fdc7761
engineershahid9028 <141816607+engineershahid9028@users.noreply.github.com>
2026-01-10 22:28:04 +0500

    Update queue.py

--- api/queue.py
+++ api/queue.py
@@ -1,1 +1,13 @@
+import os, redis, json
+
+r = redis.Redis.from_url(os.getenv("REDIS_URL"), decode_responses=True)
+QUEUE = "download_queue"
+
+def add_job(job):
+    r.rpush(QUEUE, json.dumps(job))
+
+def get_job():
+    job = r.blpop(QUEUE, timeout=5)
+    if job:
+        return json.loads(job[1])
 
