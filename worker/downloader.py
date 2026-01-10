9855865645411d0bf3b3515562513be112a9d47f
engineershahid9028 <141816607+engineershahid9028@users.noreply.github.com>
2026-01-10 23:27:46 +0500

    Update downloader.py

--- worker/downloader.py
+++ worker/downloader.py
@@ -1,8 +1,6 @@
 import time
 import re
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
+import undetected_chromedriver as uc
 
 def download_scribd(url, output, progress):
     m = re.search(r"document/(\d+)", url)
@@ -12,18 +10,18 @@ def download_scribd(url, output, progress):
     doc_id = m.group(1)
     embed_url = f"https://www.scribd.com/embeds/{doc_id}/content"
 
-    options = Options()
+    options = uc.ChromeOptions()
     options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
 
-    driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+    driver = uc.Chrome(options=options)
     driver.get(embed_url)
 
     for i in range(1, 21):
-        driver.execute_script("window.scrollBy(0, 2000)")
+        driver.execute_script("window.scrollBy(0,2000)")
         progress(i * 5)
-        time.sleep(0.4)
+        time.sleep(0.5)
 
     pdf = driver.execute_cdp_cmd("Page.printToPDF", {"printBackground": True})
     with open(output, "wb") as f:
