d48c1276deac34cef70d43c46d34a851660acc5d
engineershahid9028 <141816607+engineershahid9028@users.noreply.github.com>
2026-01-10 22:40:44 +0500

    Update bot.py

--- bot/bot.py
+++ bot/bot.py
@@ -2,25 +2,27 @@ import os
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 from api.queue import add_job
+from dotenv import load_dotenv
 
+load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 app = Application.builder().token(BOT_TOKEN).build()
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Send /download <scribd_url>")
+    await update.message.reply_text("Scribd SaaS Bot is online.\nUse /download <scribd_url>")
 
 async def download(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if not context.args:
-        return await update.message.reply_text("Usage: /download <url>")
+        return await update.message.reply_text("Usage: /download <scribd_url>")
 
     url = context.args[0]
     msg = await update.message.reply_text("📥 Added to queue...")
 
     add_job({
         "user_id": update.effective_user.id,
-        "url": url,
-        "msg_id": msg.message_id
+        "msg_id": msg.message_id,
+        "url": url
     })
 
 app.add_handler(CommandHandler("start", start))
